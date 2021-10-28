8e4490bd8eed2d2def4187847d09fdf0f2e2a75c
bikmazefe <bikmazefe@gmail.com>
2021-10-28 19:03:08 +0300

    setup router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { Link, Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Contacts from "./Contacts";
+import Signup from "./Signup";
+import Login from "./Login";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Contacts</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Contacts />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
