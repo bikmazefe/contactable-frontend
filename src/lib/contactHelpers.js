e16c17d3f48d00fe7dd7a78952c5017664911038
bikmazefe <bikmazefe@gmail.com>
2021-10-29 03:04:52 +0300

    fix search

--- src/lib/contactHelpers.js
+++ src/lib/contactHelpers.js
@@ -18,7 +18,7 @@ function search(list, query) {
 
 // exclude non-string values and timestamps
 function includeInSearch(val) {
-  return typeof val == "string" && new Date(val) === "Invalid Date";
+  return typeof val == "string" && new Date(val) == "Invalid Date";
 }
 
 export { getInitials, search };
